b173096507764203795f7eac7026418a61ad7fbc
aios <aios@aios.local>
2015-09-21 09:57:54 +0800

    Initial Commit

---
+++ LBWeChatMomentsPhotoDisplayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LBWeChatMomentsPhotoDisplayDemo
+//
+//  Created by aios on 15/9/21.
+//  Copyright (c) 2015年 BlueMobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LBWeChatMomentsPhotoDisplayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LBWeChatMomentsPhotoDisplayDemo
+//
+//  Created by aios on 15/9/21.
+//  Copyright (c) 2015年 BlueMobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
